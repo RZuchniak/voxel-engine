70e87b7736abbf98e4ace6c4d6f5eeb218bfc4e3
RZuchniak <robert.zuchniak@hotmail.com>
2025-08-28 16:32:42 -0400

    feat: create window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::sync::Arc;
+
+use winit::{
+    application::ApplicationHandler,
+    event::*,
+    event_loop::{ActiveEventLoop, EventLoop, ControlFlow},
+    keyboard::{KeyCode, PhysicalKey},
+    window::{Window, WindowId},
+};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            _ => {}
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    
+    event_loop.set_control_flow(ControlFlow::Poll);
+    
+    let mut app = App::default();
+    event_loop.run_app(&mut app);
 }
